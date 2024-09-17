915db2b9244b12270bf169e04616579d8c586bb1
NR <nicholas.rebello@gmail.com>
2024-09-16 23:59:15 -0400

    Initial Commit

---
+++ Connect4App/Connect4AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Connect4AppApp.swift
+//  Connect4App
+//
+//  Created by Nicholas Rebello on 2024-09-16.
+//
+
+import SwiftUI
+
+@main
+struct Connect4AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
